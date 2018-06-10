69cd51cf08e75539288e08a39359ce3306835cfd
rohit.jain <rohitjainitbhu.jain@gmail.com>
2018-06-10 05:50:25 +0530

    changed database to mlab

--- config/database.js
+++ config/database.js
@@ -1,3 +1,4 @@
 module.exports = {
-  'url': 'mongodb://localhost/scrum-board'
+  // 'url': 'mongodb://localhost/scrum-board'
+  'url': 'mongodb://rohitjain31:rohitjain3131@ds253960.mlab.com:53960/scrum-board'
 }
